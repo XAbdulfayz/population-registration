9818bf59b70aaaccd0ac39064352e9c19f314b4f
sevyor1 <usa.umidovna@gmail.com>
2023-09-03 16:02:54 +0500

    person create method added to personService

--- src/main/java/com/xa/populationregistration/controller/PersonController.java
+++ src/main/java/com/xa/populationregistration/controller/PersonController.java
@@ -2,10 +2,7 @@ package com.xa.populationregistration.controller;
 
 import com.xa.populationregistration.entity.Person;
 import com.xa.populationregistration.service.PersonService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/person/")
@@ -21,6 +18,11 @@ public class PersonController {
     @GetMapping("find-by-name/{name}")
     public Person findByName(@PathVariable String name){
         return service.findByName(name);
+    }
+
+    @PostMapping("create/")
+    public Person createByName(@RequestBody Person person){
+        return service.create(person);
     }
 
     @GetMapping("find-by-id/{id}")
