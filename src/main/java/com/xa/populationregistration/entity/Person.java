acd2cb458b2391d1c1aef5c149f4ad7a6396bfb3
XAbdulfayz <usa.xamidov@gmail.com>
2023-09-03 15:28:58 +0500

    Person entity updated

--- src/main/java/com/xa/populationregistration/entity/Person.java
+++ src/main/java/com/xa/populationregistration/entity/Person.java
@@ -1,9 +1,6 @@
 package com.xa.populationregistration.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Person {
@@ -11,6 +8,19 @@ public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String gender;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private Integer age;
+
 
 }
 
