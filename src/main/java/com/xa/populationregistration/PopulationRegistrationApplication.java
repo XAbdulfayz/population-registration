89816c1d281289b727e908d51b6942bfe6e49948
XAbdulfayz <usa.xamidov@gmail.com>
2023-09-03 13:52:26 +0500

    initial

---
+++ src/main/java/com/xa/populationregistration/PopulationRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.xa.populationregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopulationRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopulationRegistrationApplication.class, args);
+	}
+
+}
