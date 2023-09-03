864b5316bbd07a9a755a0338ad477f16c6334588
sevyor1 <usa.umidovna@gmail.com>
2023-09-03 15:26:56 +0500

    person repository added

--- src/main/java/com/xa/populationregistration/repository/PersonRepository.java
+++ src/main/java/com/xa/populationregistration/repository/PersonRepository.java
@@ -1,4 +1,11 @@
 package com.xa.populationregistration.repository;
 
-public interface PersonRepository {
+import com.xa.populationregistration.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+
 }
